292b9cf6c7ef59515cfef8b75ac1977b2b64ab40
reloaddev <henrik.pruess@posteo.de>
2024-04-26 12:31:56 +0200

    Allow multiple scrape runs

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, render_template
+import crochet
+crochet.setup()
+
+from flask import Flask, request, render_template, jsonify
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from scraping.spiders.chefkoch_spider import ChefkochSpider
 from scraping.spiders.zucker_jagdwurst_spider import ZuckerJagdwurstSpider
 from scraping.spiders.bianca_zapatka_spider import BiancaZapatkaSpider
 from scrapy.signalmanager import dispatcher
 from scrapy import signals
+import time
 
 app = Flask(__name__)
 
+output_data = []
+crawl_runner = CrawlerRunner()
 
 @app.route("/")
 def hello_world():
@@ -29,10 +35,21 @@ def scrape_recipe():
     spider = determine_spider(url)
     if spider is None:
         return "Unsupported Website"
-    process = CrawlerProcess()
-    process.crawl(spider, url=url)
-    process.start()  # Blocks here until items are scraped
-    return results
+    return scrape(spider,url)
+
+def scrape(spider, url):
+    scrape_with_crochet(spider, url) # Passing that URL to our Scraping Function
+    time.sleep(5) # Pause the function while the scrapy spider is running
+    return jsonify(output_data) # Returns the scraped data after being running for 20 seconds.
+
+@crochet.run_in_reactor
+def scrape_with_crochet(spider, url):
+    dispatcher.connect(_crawler_result, signal=signals.item_scraped)    
+    eventual = crawl_runner.crawl(spider, url = url)
+    return eventual
+
+def _crawler_result(item, response, spider):
+    output_data.append(dict(item))
 
 
 def determine_spider(url):
