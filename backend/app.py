ebb3cf0a2c82ca1fc1ccc53f81332ca89b9e3203
reloaddev <henrik.pruess@posteo.de>
2024-04-29 12:13:28 +0200

    Clean up code, render recipe in HTML

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 import crochet
+
 crochet.setup()
 
 from flask import Flask, request, render_template, jsonify
-import scrapy
-from scrapy.crawler import CrawlerProcess, CrawlerRunner
+from scrapy.crawler import CrawlerRunner
 from scraping.spiders.chefkoch_spider import ChefkochSpider
 from scraping.spiders.zucker_jagdwurst_spider import ZuckerJagdwurstSpider
 from scraping.spiders.bianca_zapatka_spider import BiancaZapatkaSpider
@@ -16,6 +16,7 @@ app = Flask(__name__)
 output_data = []
 crawl_runner = CrawlerRunner()
 
+
 @app.route("/")
 def hello_world():
     return render_template("app.html")
@@ -23,30 +24,20 @@ def hello_world():
 
 @app.post("/recipe")
 def scrape_recipe():
-    results = []
-
-    def crawler_results(signal, sender, item, response, spider):
-        results.append(item)
-
-    # Calls crawler_results when items are scraped
-    dispatcher.connect(crawler_results, signal=signals.item_scraped)
-
     url = request.json["url"]
     spider = determine_spider(url)
     if spider is None:
         return "Unsupported Website"
-    return scrape(spider,url)
+    scrape_with_crochet(spider, url)
+    return jsonify(output_data)
 
-def scrape(spider, url):
-    scrape_with_crochet(spider, url) # Passing that URL to our Scraping Function
-    time.sleep(5) # Pause the function while the scrapy spider is running
-    return jsonify(output_data) # Returns the scraped data after being running for 20 seconds.
 
-@crochet.run_in_reactor
+@crochet.wait_for(timeout=10.0)
 def scrape_with_crochet(spider, url):
-    dispatcher.connect(_crawler_result, signal=signals.item_scraped)    
-    eventual = crawl_runner.crawl(spider, url = url)
+    dispatcher.connect(_crawler_result, signal=signals.item_scraped)
+    eventual = crawl_runner.crawl(spider, url=url)
     return eventual
+
 
 def _crawler_result(item, response, spider):
     output_data.append(dict(item))
