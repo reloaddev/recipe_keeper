07ba7169aa1a1f222ad84f4618987304541f0e15
reloaddev <henrik.pruess@posteo.de>
2024-08-10 17:14:55 +0200

    Add SAVE indicator for saving recipes

--- frontend/src/app/recipe/[id].tsx
+++ frontend/src/app/recipe/[id].tsx
@@ -1,10 +1,13 @@
 import {Recipe} from "@/src/lib/scrape.util";
 import {useRouter} from "next/navigation";
 import {useSession} from "next-auth/react";
+import {useState} from "react";
 
 export default function RecipeView({recipe}: { recipe: Recipe }) {
     const router = useRouter();
-    const { data: session, status } = useSession();
+    const {data: session, status} = useSession();
+    const [saving, setSaving] = useState(false);
+    const [saved, setSaved] = useState(false);
 
     const title = recipe.title;
     const ingredientList = recipe.ingredients;
@@ -14,6 +17,8 @@ export default function RecipeView({recipe}: { recipe: Recipe }) {
     const formattedInstructions = instructions?.replaceAll("<br>", "\n").trimEnd();
 
     async function saveRecipe() {
+        if (saved) return;
+        setSaving(true);
         await fetch("/api", {
             method: 'POST',
             mode: 'cors',
@@ -24,7 +29,11 @@ export default function RecipeView({recipe}: { recipe: Recipe }) {
                 url: recipe.url,
                 userId: session?.user?.id
             })
-        })
+        }).catch(() => setSaving(false));
+        setTimeout(() => {
+            setSaving(false);
+            setSaved(true);
+        }, 1500)
     }
 
     return (
@@ -34,10 +43,13 @@ export default function RecipeView({recipe}: { recipe: Recipe }) {
             <ul>{ingredientItems}</ul>
             <h1 className="text-xl mt-7">Instructions</h1>
             <div className="display-linebreak">{formattedInstructions}</div>
-            {status === "authenticated" && <button onClick={saveRecipe}
-                                       className="w-1/3 sm:w-1/12 mt-5 text-white bg-violet rounded-lg p-2 sm:p-3 ml-auto">
-                Save
-            </button>}
+            {
+                status === "authenticated" &&
+                <button onClick={saveRecipe}
+                        className="w-1/3 sm:w-1/12 mt-5 text-white bg-violet rounded-lg p-2 sm:p-3 ml-auto">
+                    {saved? "Saved!" : (saving ? "Saving..." : "Save")}
+                </button>
+            }
             {status === "unauthenticated" &&
                 <p className="mt-5 text-violet ml-auto"
                    onClick={() => router.push('/')}>
