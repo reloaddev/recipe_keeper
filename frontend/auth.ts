6bb95300e606cf48131c8621ac21cd34b518d5f3
reloaddev <henrik.pruess@posteo.de>
2024-08-10 11:48:45 +0200

    Connect Auth.js to Prisma/DB

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from "next-auth"
 import Google from "@auth/core/providers/google";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [Google],
-})
+});
