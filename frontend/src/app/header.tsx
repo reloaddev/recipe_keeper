9cc83c2a2fa409cc6bf09548cc99a5aa13a9f0f6
reloaddev <henrik.pruess@posteo.de>
2024-08-11 22:10:54 +0200

    Indicate auth state in header - add auth and unauthed user icon to header - add tooltips to icons

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import {Menu} from "react-feather";
+import {Menu, UserCheck, UserMinus} from "react-feather";
 import {useRouter} from "next/navigation";
 import {Sidenav} from "@/src/app/components/Sidenav";
 import {useState} from "react";
+import {useSession} from "next-auth/react";
+import { Tooltip } from 'react-tooltip'
+
 
 export default function Header() {
-
+    const {data: session, status} = useSession();
     const router = useRouter();
     const [showSidebar, setShowSidebar] = useState(false);
 
@@ -14,6 +17,12 @@ export default function Header() {
         <header className="flex justify-between items-center bg-violet h-16 px-5 sm:px-16 py-8 sm:py-10">
             <h1 onClick={() => router.push("/")} className="text-2xl text-white">Recipe Scraper</h1>
             <div className="flex gap-4">
+                {status === "authenticated"
+                    ? <UserCheck size={30} color="white" data-tooltip-id="authed-user" data-tooltip-content={`Signed in as ${session?.user?.name}`}/>
+                    : <UserMinus size={30} color="white" data-tooltip-id="unauthed-user" data-tooltip-content="Not signed in"/>
+                }
+                <Tooltip id="authed-user" place="bottom"/>
+                <Tooltip id="unauthed-user" place="bottom"/>
                 <Menu onClick={() => setShowSidebar(oldVal => !oldVal)} size={30} color="white"/>
             </div>
             <Sidenav show={showSidebar} setter={setShowSidebar}/>
